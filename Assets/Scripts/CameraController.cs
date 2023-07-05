f9d834ebf5f73e304c46aaca8a36865a70f9d8e9
Nickita45 <supernikita45@gmail.com>
2023-07-05 21:49:41 +0200

    added changes to CameraController, now move is working correct for camera and some invisible border

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,64 +10,102 @@ public class CameraController : MonoBehaviour
     private GameObject _cameraParent;
     [SerializeField]
     private Transform _targetObject;
+    [SerializeField]
+    private GameObject mainObject;
 
     [SerializeField]
     private float _speed = 5f;
     [SerializeField]
     private float _speedRotation = 5f;
 
-    private bool isRotating = false;
-    private Quaternion targetRotation;
-    public Vector3 rotationAxis = Vector3.up;
-    // Start is called before the first frame update
-    void Start()
+    private bool _isRotating = false;
+    private Quaternion _targetRotation;
+    private readonly Vector3 rotationAxis = Vector3.up;
+    
+    private void Start()
     {
-        
+        ChangePositionForCamera();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        HandleMovementInput();
+        HandleRotationInput(KeyCode.E, -90f);
+        HandleRotationInput(KeyCode.Q, 90f); 
+        HandleRotation();
+    }
+
+    private void HandleMovementInput()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = -Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(verticalInput, 0f, horizontalInput) * _speed * Time.deltaTime;
 
-        _cameraParent.transform.position += movement;
-
-        if (Input.GetKeyDown(KeyCode.Space) && !isRotating)
+        movement = _cameraParent.transform.TransformDirection(movement);
+        Vector3 newPosition = mainObject.transform.position + movement;
+    
+        // Check if the new position is within the boundary
+        if (IsWithinBoundary(newPosition))
         {
-            targetRotation = _camera.transform.rotation * Quaternion.AngleAxis(90f, rotationAxis);
-
-            isRotating = true;
+            mainObject.transform.position = newPosition;
         }
-
-        if (isRotating)
-        {
-            Debug.Log(targetRotation);
-            Debug.Log(Quaternion.Angle(_camera.transform.rotation, targetRotation));
-            _camera.transform.RotateAround(_targetObject.position, rotationAxis, _speedRotation * Time.deltaTime);
-
-            if (Quaternion.Angle(_camera.transform.rotation, targetRotation) < 1f)
-            {
-                isRotating = false;
-            }
-        }
-
+        
     }
-    IEnumerator Rotation()
+    private bool IsWithinBoundary(Vector3 position)
     {
-        while(true)
+        // Making the width more distance to working camera 
+        float widthValuePlus = 3f;
+        // Define the center and width of the boundary
+        Vector3 boundaryCenter = GameManagerMap.Instance.MainParent.transform.position;
+        boundaryCenter.x += 5;
+        float boundaryWidth = GameManagerMap.Instance.Map.width + widthValuePlus;
+
+        // Calculate the minimum and maximum values for the x and z coordinates of the boundary
+        float minX = boundaryCenter.x - boundaryWidth / 2f;
+        float maxX = boundaryCenter.x + boundaryWidth / 2f;
+        float minZ = boundaryCenter.z - boundaryWidth / 2f;
+        float maxZ = boundaryCenter.z + boundaryWidth / 2f;
+
+        // Check if the position is within the boundary
+        if (position.x >= minX && position.x <= maxX && position.z >= minZ && position.z <= maxZ)
         {
-            _camera.transform.Rotate(_targetObject.transform.position, _speedRotation);
-
-//            if (_camera.transform.eulerAngles == targetRotation)
-            {
-                break;
-            }
-            yield return new WaitForSeconds(0.05f);
+            return true;
         }
-        isRotating = false;
 
+        return false;
+    }
+    private void HandleRotationInput(KeyCode keyCode, float angle)
+    {
+        if (Input.GetKeyDown(keyCode) && !_isRotating)
+        {
+            _targetRotation = _cameraParent.transform.rotation * Quaternion.AngleAxis(angle, rotationAxis);
+            _isRotating = true;
+        }
+    }
+    private void HandleRotation()
+    {
+        if (_isRotating)
+        {
+            Vector3 directionToTarget = _targetObject.position - _cameraParent.transform.position;
+            Vector3 rotatedDirection = _targetRotation * directionToTarget;
+
+            float step = _speedRotation * Time.deltaTime;
+            _cameraParent.transform.rotation = Quaternion.RotateTowards(_cameraParent.transform.rotation, _targetRotation, step);
+
+            ChangePositionForCamera();
+
+            if (Quaternion.Angle(_cameraParent.transform.rotation, _targetRotation) < 0.01f)
+            {
+                _isRotating = false;
+                _cameraParent.transform.rotation = _targetRotation;
+            }
+        }
+    }
+    public void ChangePositionForCamera()
+    {
+        Vector3 newPositionForRotate = _targetObject.position - _camera.transform.forward * Vector3.Distance(_targetObject.position, _camera.transform.position);
+        newPositionForRotate.y = _cameraParent.transform.position.y;
+        _cameraParent.transform.position = newPositionForRotate;
     }
 }
