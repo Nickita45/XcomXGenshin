b01e8f56ed61a84c9930469d3178c340c1184f49
cofee-on-the-desk <89153225+cofee-on-the-desk@users.noreply.github.com>
2023-07-13 17:49:56 +0300

    Limit camera movement in bounds

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,7 +12,7 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private Transform _targetObject;
     [SerializeField]
-    private GameObject mainObject;
+    private GameObject _mainObject;
 
     [SerializeField]
     private float _speed = 5f;
@@ -21,17 +22,17 @@ public class CameraController : MonoBehaviour
     private bool _isRotating = false;
     private Quaternion _targetRotation;
     private readonly Vector3 rotationAxis = Vector3.up;
-    
+
     private void Start()
     {
-        ChangePositionForCamera();
+        //ChangePositionForCamera();
     }
 
     private void Update()
     {
         HandleMovementInput();
         HandleRotationInput(KeyCode.E, -90f);
-        HandleRotationInput(KeyCode.Q, 90f); 
+        HandleRotationInput(KeyCode.Q, 90f);
         HandleRotation();
     }
 
@@ -43,37 +44,39 @@ public class CameraController : MonoBehaviour
         Vector3 movement = new Vector3(verticalInput, 0f, horizontalInput) * _speed * Time.deltaTime;
 
         movement = _cameraParent.transform.TransformDirection(movement);
-        Vector3 newPosition = mainObject.transform.position + movement;
+        Vector3 newPosition = _mainObject.transform.position + movement;
 
-        // Check if the new position is within the boundary
         IsWithinBoundary(ref newPosition);
-        mainObject.transform.position = newPosition;
-
+        _mainObject.transform.position = newPosition;
     }
+
     private void IsWithinBoundary(ref Vector3 position)
     {
-        // Making the width more distance to working camera 
-        float widthValuePlus = 8f;
-        // Define the center and width of the boundary
-        Vector3 boundaryCenter = GameManagerMap.Instance.MainParent.transform.position;
-        float boundaryWidth = GameManagerMap.Instance.Map.width + widthValuePlus;
+        Vector3 center = transform.position;
+        (float width, float height) = (GameManagerMap.Instance.Map.width, GameManagerMap.Instance.Map.width);
+        float cameraHeight = position.y - transform.position.y;
 
-        // Calculate the minimum and maximum values for the x and z coordinates of the boundary
-        (float minX, float maxX, float minZ, float maxZ) maxPos = (boundaryCenter.x - boundaryWidth / 2 , boundaryCenter.x + boundaryWidth ,
-                                                                    boundaryCenter.z - boundaryWidth , boundaryCenter.z + boundaryWidth );
-        // Check if the position is within the boundary
-        if (position.x < maxPos.minX || position.x > maxPos.maxX)
-        {
-            position.x = mainObject.transform.position.x;
-        }
+        // Find the difference vector between the projected point
+        // and the camera position (assuming the camera angle is 45 degrees)
+        Vector3 diff = _camera.transform.forward * cameraHeight * Mathf.Sqrt(2);
 
-        if(position.z < maxPos.minZ || position.z > maxPos.maxZ)
-        {
-            position.z = mainObject.transform.position.z;
-        }
+        // Find the coordinates of the projected point
+        Vector3 projected = position + diff;
+        //Debug.DrawRay(position, _camera.transform.forward * 8f, Color.red, duration: 1f);
 
-        
+        // Create the bounding rect
+        Rect bounds = new(center.x - width / 2f, center.z - height / 2f, width, height);
+
+        // Keep the position inside bounds
+        if (projected.x < bounds.xMin) projected.x = bounds.xMin;
+        else if (projected.x > bounds.xMax) projected.x = bounds.xMax;
+
+        if (projected.z < bounds.yMin) projected.z = bounds.yMin;
+        else if (projected.z > bounds.yMax) projected.z = bounds.yMax;
+
+        position = projected - diff;
     }
+
     private void HandleRotationInput(KeyCode keyCode, float angle)
     {
         if (Input.GetKeyDown(keyCode) && !_isRotating)
@@ -92,7 +95,7 @@ public class CameraController : MonoBehaviour
             float step = _speedRotation * Time.deltaTime;
             _cameraParent.transform.rotation = Quaternion.RotateTowards(_cameraParent.transform.rotation, _targetRotation, step);
 
-            ChangePositionForCamera();
+            //ChangePositionForCamera();
 
             if (Quaternion.Angle(_cameraParent.transform.rotation, _targetRotation) < 0.01f)
             {
@@ -101,10 +104,10 @@ public class CameraController : MonoBehaviour
             }
         }
     }
-    public void ChangePositionForCamera()
+    /*public void ChangePositionForCamera()
     {
         Vector3 newPositionForRotate = _targetObject.position - _camera.transform.forward * Vector3.Distance(_targetObject.position, _camera.transform.position);
         newPositionForRotate.y = _cameraParent.transform.position.y;
         _cameraParent.transform.position = newPositionForRotate;
-    }
+    }*/
 }
